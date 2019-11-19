44dae6d63ad284fd27d007a5f326d205fbe68cc4
hyungi <tjdgusrlek@gmail.com>
2019-11-19 10:15:35 +0900

    add() 함수 추가 및 테스트

--- chapter_git/hello_python.py
+++ chapter_git/hello_python.py
@@ -1,3 +1,11 @@
+def add(a, b):
+    return a + b
+
+
 if __name__ == "__main__":
     print("Hello python")
     print("hi pycharm")
+    x = 10
+    y = 5
+    res = add(x, y)
+    print("add({}, {}) result => {}".format(x, y, res))
