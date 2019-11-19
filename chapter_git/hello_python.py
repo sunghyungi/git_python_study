bf08deee405639d20bfc8c35f668c4a1b388bd5d
hyungi <tjdgusrlek@gmail.com>
2019-11-19 10:34:56 +0900

    div(a, b) 함수 추가, main print문 변경

--- chapter_git/hello_python.py
+++ chapter_git/hello_python.py
@@ -2,10 +2,13 @@ def add(a, b):
     return a + b
 
 
+def div(a, b):
+    return a / b
+
+
 if __name__ == "__main__":
     print("Hello python")
     print("hi pycharm")
     x = 10
     y = 5
-    res = add(x, y)
-    print("add({}, {}) result => {}".format(x, y, res))
+    print("add({}, {}) result => {}\ndiv({}, {}) = {}".format(x, y, add(x, y), x, y, div(x, y)))
