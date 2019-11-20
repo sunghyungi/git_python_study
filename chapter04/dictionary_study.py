3acaa26d29c2b3d743cfbb4f999fa3e564d51bec
sunghyungi <tjdgusrlek@gmail.com>
2019-11-20 10:38:48 +0900

    dictionary 연습, 활용

--- chapter04/dictionary_study.py
+++ chapter04/dictionary_study.py
@@ -1,3 +1,20 @@
+def dict_app():
+    fruit_code = {'사과': 101, '배': 102, '딸기': 103, '포도': 104, '바나나': 105}
+    print(fruit_code)
+    print(fruit_code.keys())
+    print(fruit_code.values())
+    print(fruit_code.items())
+    print(list(fruit_code.keys()))
+    print(list(fruit_code.values()))
+    print(list(fruit_code.items()))
+    fruit_code2 = dict(오렌지=106, 수박=107)
+    fruit_code.update(fruit_code2)
+    print(fruit_code)
+    fruit_code2.clear()
+    print(fruit_code2)
+    print(type(fruit_code2))
+
+
 if __name__ == "__main__":
     country_capital = {"영국": "런던", "프랑스": "파리", "스위스": "베른", "호주": "캔버라", "덴마크": "코펜하겐"}
 
@@ -26,3 +43,5 @@ if __name__ == "__main__":
     print(country_capital)
     del country_capital["호주"]
     print(country_capital)
+
+    dict_app()
