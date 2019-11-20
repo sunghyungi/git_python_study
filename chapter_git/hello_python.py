50cf4c0cc2c9aea1f670c83e0c88997d53c70f0d
sunghyungi <tjdgusrlek@gmail.com>
2019-11-20 10:39:25 +0900

    함수 연습

--- chapter_git/hello_python.py
+++ chapter_git/hello_python.py
@@ -10,22 +10,6 @@ def mul(a, b):
     return a * b
 
 
-def dict_app():
-    fruit_code = {'사과': 101, '배': 102, '딸기': 103, '포도': 104, '바나나': 105}
-    print(fruit_code)
-    print(fruit_code.keys())
-    print(fruit_code.values())
-    print(fruit_code.items())
-    print(list(fruit_code.keys()))
-    print(list(fruit_code.values()))
-    print(list(fruit_code.items()))
-    fruit_code2 = dict(오렌지=106, 수박=107)
-    fruit_code.update(fruit_code2)
-    print(fruit_code)
-    fruit_code2.clear()
-    print(fruit_code2)
-    print(type(fruit_code2))
-
 
 if __name__ == "__main__":
     print("{} {}".format('hello python', 'hi pycharm'))
@@ -35,4 +19,3 @@ if __name__ == "__main__":
     print("mul({}, {}) = {}".format(x, y, mul(x, y)))
     print("div({}, {}) = {}".format(x, y, div(x, y)))
 
-    dict_app()
